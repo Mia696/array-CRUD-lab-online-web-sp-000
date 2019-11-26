69a7e7162fb0afd2a4e666f5279f2439cb065d6b
Mia <tamiapaschall@gmail.com>
2019-11-26 18:08:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  example.push("green")
+  example.push("Green")
 end
 
 def add_element_to_start_of_array(array, element)
